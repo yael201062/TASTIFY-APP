681f9431612f22396bca53bda93f0689c0ecfd1d
yael201062 <yael201062@gmail.com>
2025-03-29 18:27:18 +0300

    user already logged in before

---
+++ app/src/main/java/com/example/tastify/ui/splash/SplashFragment.kt
@@ -0,0 +1,27 @@
+package com.example.tastify.ui.splash
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.tastify.R
+import com.google.firebase.auth.FirebaseAuth
+
+class SplashFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        return inflater.inflate(R.layout.fragment_splash, container, false)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        val currentUser = FirebaseAuth.getInstance().currentUser
+        if (currentUser != null) {
+            findNavController().navigate(R.id.action_splashFragment_to_homeFragment)
+        } else {
+            findNavController().navigate(R.id.action_splashFragment_to_loginFragment)
+        }
+    }
+}
